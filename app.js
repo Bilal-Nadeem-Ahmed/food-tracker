a790430a984a1fa38bf5d09d02443e5feca6bb44
Bilal-Nadeem-Ahmed <61297097+Megamind44@users.noreply.github.com>
2020-11-10 00:04:24 +0000

    Update app.js

--- app.js
+++ app.js
@@ -20,11 +20,17 @@ class Food{
   
 };
 async function geFood(){
-  const response=await fetch(`https://api.nal.usda.gov/fdc/v1/foods/search?api_key=09EB3dIbSzfsmSwlykptslh9U1c8eNvNnKo9SfT0&query=Cheddar%20Cheese`)
+ // const response=await fetch(`https://api.nal.usda.gov/fdc/v1/foods/search?api_key=09EB3dIbSzfsmSwlykptslh9U1c8eNvNnKo9SfT0&query=Cheddar%20Cheese&pageSize=10&pageNumber=1& measure=100`)
+ const response=await fetch(`https://api.nal.usda.gov/fdc/v1/food/380864?api_key=09EB3dIbSzfsmSwlykptslh9U1c8eNvNnKo9SfT0`)
 
   const responseData = await response.json()
   console.log(responseData)
   return responseData;
 };
 geFood();
+
+// so i need to make an initial request which returns the search query. this is limited to 10 items.
+// to get the indavidual info the second get request must be made using the id from the first request, it is specified after the question mark and returns the info we need. it also returns an average value for portion size, we need to be able to adjust this after selection, which should automatically adjust the other quantities. we will continue with this tmrw!
+
+// first step for tmrw is displaying the info in a list and onclick events for each of the ten maybe with a foreach loop, use the id to select and then make the  next request which should get back data!
 
