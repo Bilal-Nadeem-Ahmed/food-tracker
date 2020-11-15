4b0c9eff512bff357cdd4f4ebf06bb266aefd633
Bilal-Nadeem-Ahmed <61297097+Megamind44@users.noreply.github.com>
2020-11-15 00:55:31 +0000

    added a second function to start structoring doc

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const  uiSelectors = {
  list :".list",
  addToList : ".results",
  searchItem :".searchitem",
+ mealsDiv: ".mealsdiv",
+ addDayBtn: "#addDaybtn",
+ searchDiv: ".searchdiv",
  apiKey :"hdg4q8tdGBR4tIeH4r14i9Zk1TcwYiKCxe2usGxr",}
  
 
@@ -48,7 +51,7 @@ async function getFoodItem(id){
   
   const responses=await fetch(`https://api.nal.usda.gov/fdc/v1/food/${id}?api_key=09EB3dIbSzfsmSwlykptslh9U1c8eNvNnKo9SfT0`);
  
-   const responsesData = await responses.json()
+   const responsesData = await responses.json().catch(err=> console.log(err))
   console.log(responsesData)
 
   //uiSelectors.addToList.style.display="block";  
@@ -60,7 +63,27 @@ async function getFoodItem(id){
   
   }
 
-uiSelectors.submitBtn.addEventListener("click", function(e){
+
+ 
+
+// create a class that will be used onclick to create a new date. the id of the class will me dinamically calculated as will part of the  classname. it will give you an option to add a meal to it , using an eventlistner on the button and if done so an input field will arrive, we will then use the function to show the list of search items and then once clicked we can add that item to the list. once that is done we can add multiple meals for the day and also edit each item and its content . we can use haschildnode to see if there is an element there already. then use a loop to get through to the end and add a new date, same for the list item
+
+// hide the search box
+document.querySelector(uiSelectors.searchDiv).style.display= "none";
+// event listner for add food
+document.querySelector(uiSelectors.addDayBtn).addEventListener("click", function(e){
+  // show input field 
+  document.querySelector(uiSelectors.searchDiv).style.display= "block";
+  // hide the add day button
+  document.querySelector(uiSelectors.addDayBtn).style.display="none";
+
+
+  
+  e.preventDefault()
+})
+
+ // event listner for search
+ uiSelectors.submitBtn.addEventListener("click", function(e){
   
   // check if there is any data in input field
   if(uiSelectors.inputval.value===""){
@@ -74,4 +97,44 @@ uiSelectors.submitBtn.addEventListener("click", function(e){
 })
 
 
+// this function checks for other food divs which are used to store daily meal info, it returns the new div which should be used to in another function and create a breakfast lunch and dinner element which have event listners to add food items to it
+function addDay(){
+  // get the meals div container
+  let mealsDiv = document.querySelector(uiSelectors.mealsDiv);
+
+  // create a div
+  let newDiv=document.createElement("div");
+
+  // check if there are any any others meals if so create the right classname  
+  if (document.querySelectorAll(".foodDiv")===null){
+    newDiv.className=`foodDiv fd-0`;
+    newDiv.id="fd-0"
+
+  } else {
+    newDiv.className=`foodDiv fd-${document.querySelectorAll(".foodDiv").length+1}`;
+    newDiv.id= `fd-${document.querySelectorAll(".foodDiv").length+1}`;
+
+  }
+  // put the new meal in the meals container 
+  mealsDiv.appendChild(newDiv)
+
+ //return addmeals(newDiv);
+ return newDiv;
  
+ 
+}
+// need to add an element which once it is clicked we launch
+
+function addmeals(newDiv){
+  
+  let breakfastDiv= document.createElement("div");
+  breakfastDiv.className="breakfast";
+  let breakfastP = document.createElement("p");
+  breakfastP.innerText = "Breakfast";
+  newDiv.appendChild(breakfastDiv);
+  breakfastDiv.appendChild(breakfastP);
+
+}
+
+//addmeals(document.getElementById("#fd-1"));
+// the above function needs to be completed. it works but as of yet does not do much appart from show the word breakfastin a paragraph in a breakfast div which is a child of the div the previous function it was connected to. i can imagine we would use this function in the return statement of the above div. we need to find a way to create a new ul undre this and when ever we click on the name of the meal , we add food items to the ul of that meal. that may work by changing the parameters of this function and the second function which gets data back from the id of the api, this may need to be nested in that function in order to be able to access all of its info, or just called in th =e return statement and passed that perticular vlue which we could use to add the info we need to the li's . also it will be a task to figure out how we will update the calories but we can deal with that later on. we also need to figure out how we will populate the data , maybe by making some of these functions in to one then it will work.?
